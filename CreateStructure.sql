d6c1c10d8e20d638aad32526478051dbd888f9bf
Кирилл Федоров <hxxxrz@gmail.com>
2023-01-09 14:28:24 +0300

    Создание таблиц, первая версия

---
+++ CreateStructure.sql
@@ -0,0 +1,29 @@
+if object_id('dbo.SKU', 'U') is not null
+  drop table dbo.SKU;
+  
+create table dbo.SKU (
+  ID int primary key identity,
+  Code as concat('s', ID) unique,
+  Name varchar(255)
+);
+
+if object_id('dbo.Basket', 'U') is not null
+  drop table dbo.SKU;
+
+create table dbo.Basket (
+  ID int primary key identity,
+  ID_SKU int references dbo.SKU(ID),
+  Quantity int check(Quantity >= 0),
+  Value dec(18, 2) check(Value >= 0),
+  PurchaseDate date default getdate(),
+  DiscountValue dec(18, 2) default 0.00
+);
+
+if object_id('dbo.Family', 'U') is not null
+  drop table dbo.SKU;
+
+create table dbo.Family (
+  ID int primary key identity,
+  SurName varchar(255) unique,
+  BudgetValue dec(18, 2)
+);
