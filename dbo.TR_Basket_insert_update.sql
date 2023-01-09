cd3766501b627a53696d7ce3635b2570c3dc1f0b
Кирилл Федоров <hxxxrz@gmail.com>
2023-01-09 14:55:30 +0300

    Триггеры, первый вариант

---
+++ dbo.TR_Basket_insert_update.sql
@@ -0,0 +1,19 @@
+if object_id('dbo.TR_Basket_insert_update', 'U') is not null  
+   drop trigger dbo.TR_Basket_insert_update;
+   
+create trigger dbo.TR_Basket_insert_update
+  on dbo.Basket after insert as
+    begin
+      declare @countIdSku table(ID_SKU int, count_value int)
+      
+      insert into @countIdSku (ID_SKU, count_value)
+        select ID_SKU, count(*) as count_value
+          from dbo.Basket
+          group by ID_SKU
+          having count(*) > 1
+        
+        update dbo.Basket
+          set DiscountValue = Value * 0.05
+            from @countIdSku as c
+            where c.ID_SKU = dbo.Basket.ID_SKU
+    end;
